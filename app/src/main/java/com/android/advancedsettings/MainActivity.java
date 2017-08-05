1f4b2102b2137d9b869a1e8fe6a93eff0321146a
lin666 <870818220@qq.com>
2017-08-05 15:06:00 +0800

    update

--- app/src/main/java/com/android/advancedsettings/MainActivity.java
+++ app/src/main/java/com/android/advancedsettings/MainActivity.java
@@ -3,10 +3,11 @@ package com.android.advancedsettings;
 import android.app.Activity;
 import android.os.Bundle;
 import android.preference.Preference;
-import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
 import android.preference.PreferenceScreen;
 import android.preference.SwitchPreference;
 import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -14,68 +15,75 @@ import android.view.WindowManager;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
-public class MainActivity extends PreferenceActivity implements Preference.OnPreferenceChangeListener, Preference.OnPreferenceClickListener
-{
-    private SwitchPreference DoubleTap;
-    private SwitchPreference Charging;
-
+public class MainActivity extends AppCompatActivity {
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Miui(this,true);
         Flyme(this,true);
         super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.pref_settings);
-        init();
+        setContentView(R.layout.activity);
+        getFragmentManager().beginTransaction().replace(R.id.settings, new SettingsFragment()).commit();
     }
 
-    public void onPostResume()
-    {
-        super.onPostResume();
-        if (Settings.System.getInt(getContentResolver(), "double_tap_enable", 0) == 1){
-            DoubleTap.setChecked(true);
-        } else {
-            DoubleTap.setChecked(false);
-        }
-        if (Settings.System.getInt(getContentResolver(), "CHARGING_LIGHT_PULSE", 0) == 1){
-            Charging.setChecked(true);
-        } else {
-            Charging.setChecked(false);
+    public class SettingsFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener, Preference.OnPreferenceClickListener {
+
+        private SwitchPreference DoubleTap;
+        private SwitchPreference Charging;
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.pref_settings);
+            init();
         }
 
-    }
-
-    public void init() {
-        DoubleTap = (SwitchPreference) findPreference("double_tap");
-        DoubleTap.setOnPreferenceChangeListener(this);
-        Charging = (SwitchPreference) findPreference("Charging");
-        Charging.setOnPreferenceChangeListener(this);
-    }
-
-    public boolean onPreferenceChange(Preference Preference, Object Object){
-        return true;
-    }
-
-    public boolean onPreferenceClick(Preference Preference)
-    {
-        return false;
-    }
-
-    public boolean onPreferenceTreeClick(PreferenceScreen PreferenceScreen, Preference Preference)
-    {
-        if (Preference == DoubleTap) {
-            if (DoubleTap.isChecked()) {
-                Settings.System.putInt(getContentResolver(), "double_tap_enable", 1);
+        public void onResume() {
+            super.onResume();
+            if (Settings.System.getInt(getContentResolver(), "double_tap_enable", 0) == 1) {
+                DoubleTap.setChecked(true);
             } else {
-                Settings.System.putInt(getContentResolver(), "double_tap_enable", 0);
+                DoubleTap.setChecked(false);
             }
-        } else if (Preference == Charging) {
-            if (Charging.isChecked()) {
-                Settings.System.putInt(getContentResolver(), "CHARGING_LIGHT_PULSE", 1);
+            if (Settings.System.getInt(getContentResolver(), "CHARGING_LIGHT_PULSE", 0) == 1) {
+                Charging.setChecked(true);
             } else {
-                Settings.System.putInt(getContentResolver(), "CHARGING_LIGHT_PULSE", 0);
+                Charging.setChecked(false);
             }
+
         }
-        return false;
+
+        public void init() {
+            DoubleTap = (SwitchPreference) findPreference("double_tap");
+            DoubleTap.setOnPreferenceChangeListener(this);
+            Charging = (SwitchPreference) findPreference("Charging");
+            Charging.setOnPreferenceChangeListener(this);
+        }
+
+        public boolean onPreferenceChange(Preference Preference, Object Object) {
+            return true;
+        }
+
+        public boolean onPreferenceClick(Preference Preference) {
+            return false;
+        }
+
+        public boolean onPreferenceTreeClick(PreferenceScreen PreferenceScreen, Preference Preference) {
+            if (Preference == DoubleTap) {
+                if (DoubleTap.isChecked()) {
+                    Settings.System.putInt(getContentResolver(), "double_tap_enable", 1);
+                } else {
+                    Settings.System.putInt(getContentResolver(), "double_tap_enable", 0);
+                }
+            } else if (Preference == Charging) {
+                if (Charging.isChecked()) {
+                    Settings.System.putInt(getContentResolver(), "CHARGING_LIGHT_PULSE", 1);
+                } else {
+                    Settings.System.putInt(getContentResolver(), "CHARGING_LIGHT_PULSE", 0);
+                }
+            }
+            return false;
+        }
     }
 
     public boolean Miui(Activity activity, boolean dark) {
@@ -98,26 +106,24 @@ public class MainActivity extends PreferenceActivity implements Preference.OnPre
     }
 
     public boolean Flyme(Activity activity, boolean dark) {
-            try {
-                WindowManager.LayoutParams lp = activity.getWindow().getAttributes();
-                Field darkFlag = WindowManager.LayoutParams.class.getDeclaredField("MEIZU_FLAG_DARK_STATUS_BAR_ICON");
-                Field FlymeFlags = WindowManager.LayoutParams.class.getDeclaredField("meizuFlags");
-                darkFlag.setAccessible(true);
-                FlymeFlags.setAccessible(true);
-                int bit = darkFlag.getInt(null);
-                int value = FlymeFlags.getInt(lp);
-                if (dark) {
-                    value |= bit;
-                } else {
-                    value &= ~bit;
-                }
-                FlymeFlags.setInt(lp, value);
-                activity.getWindow().setAttributes(lp);
-                return true;
-            } catch (Exception e) {
-                return false;
+        try {
+            WindowManager.LayoutParams lp = activity.getWindow().getAttributes();
+            Field darkFlag = WindowManager.LayoutParams.class.getDeclaredField("MEIZU_FLAG_DARK_STATUS_BAR_ICON");
+            Field FlymeFlags = WindowManager.LayoutParams.class.getDeclaredField("meizuFlags");
+            darkFlag.setAccessible(true);
+            FlymeFlags.setAccessible(true);
+            int bit = darkFlag.getInt(null);
+            int value = FlymeFlags.getInt(lp);
+            if (dark) {
+                value |= bit;
+            } else {
+                value &= ~bit;
             }
+            FlymeFlags.setInt(lp, value);
+            activity.getWindow().setAttributes(lp);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
-
 }
-
